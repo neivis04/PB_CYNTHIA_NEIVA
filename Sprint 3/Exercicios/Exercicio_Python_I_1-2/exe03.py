d8033afbe32b9f0ceaf439f6e19497e7832712a2
neivis04 <163541851+neivis04@users.noreply.github.com>
2024-10-21 22:45:17 -0300

    Add files via upload
    Subindo diretório da sprint 03 para o repositório

---
+++ Sprint 3/Exercicios/Exercicio_Python_I_1-2/exe03.py
@@ -0,0 +1,6 @@
+# Declarando a variável 'numeros' com a lista de números pares de 0 até 20
+numeros = list(range(0, 21, 2))  # A função range() vai de 0 a 20 com passo 2, ou seja, só pares
+
+# Imprimindo cada número da lista 'numeros'
+for numero in numeros:
+    print(numero)
