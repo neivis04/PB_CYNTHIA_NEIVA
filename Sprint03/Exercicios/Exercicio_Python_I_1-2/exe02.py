ab860ab4e60529772efbceca5313a3f551a913fa
neivis04 <163541851+neivis04@users.noreply.github.com>
2024-10-21 22:42:52 -0300

    Delete Sprint03 directory

--- Sprint03/Exercicios/Exercicio_Python_I_1-2/exe02.py
+++
@@ -1,9 +0,0 @@
-# Declarando a variável 'numeros' como uma lista usando range()
-numeros = list(range(2, 5))  # Adiciona os números 2, 3 e 4 à lista
-
-# Verificando se cada número é par ou ímpar e imprimindo o resultado
-for numero in numeros:
-    if numero % 2 == 0:
-        print(f"Par: {numero}")
-    else:
-        print(f"Ímpar: {numero}")
--- Sprint03/Exercicios/Exercicio_Python_I_1-2/exe05.py
+++
@@ -1,7 +0,0 @@
-# Declarando as variáveis
-dia = 22
-mes = 10
-ano = 2022
-
-# Imprimindo a data no formato dia/mes/ano
-print(f"{dia}/{mes}/{ano}")
